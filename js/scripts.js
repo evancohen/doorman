80b6f1ee236950a8890f16a1b4ebbc27b3ec3567
Evan Cohen <evanbtcohen@gmail.com>
2018-12-11 03:47:52 -0800

    visual improvments

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 let $AppendKey = key => {
     let options = { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
-    let valid = (Date.now() <= Date.parse(key.expiry)) ? 'valid' : 'expired'
-    $codes.append(`<tr id="${key['_id']}"><td>${key.name}</td>  
-        <td class="${valid}">${new Date(key.expiry).toLocaleDateString('en-US', options)}</td>
+    let valid = (Date.now() <= Date.parse(key.expiry)) ? 'table-active' : 'table-danger'
+    $codes.append(`<tr id="${key['_id']}" class="${valid}"><td>${key.name}</td>  
+        <td>${new Date(key.expiry).toLocaleDateString('en-US', options)}</td>
         <td>[${key.pin}]</td>
-        <td><a href="#" onclick="deleteCode('${key['_id']}')">X</a></dt></tr>`)
+        <td><a href="#" onclick="deleteCode('${key['_id']}')">🗑️</a></dt></tr>`)
 }
 
 // On Load...
@@ -42,7 +42,8 @@ let generateCode = () => {
     DB.generateCode($("#name").val(), computedDate.toISOString()).done(function (result) {
         $AppendKey(result)
         $a = $("<a>").attr("href", `sms:?body=${result.pin}`)
-        $a.text(result.pin)
+        $a.attr("class", "btn btn-sm btn-outline-success col-12")
+        $a.text("💬 Send as SMS: " + result.pin)
         $("#generated-code").html($a)
     })
 }
