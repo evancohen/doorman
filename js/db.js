230bc79bf0889dfe2b2c792b6e9700866a47b878
Evan Cohen <evanbtcohen@gmail.com>
2018-12-10 23:00:31 -0800

    creating UI

---
+++ js/db.js
@@ -0,0 +1,31 @@
+const API_KEY = "5c0f48fb5c41c52f7e131f3a";
+const URL = "https://doorman-1f48.restdb.io/rest/keys"
+const HEADERS = {
+    "content-type": "application/json",
+    "x-apikey": API_KEY,
+    "cache-control": "no-cache"
+}
+
+let DB = {
+    getCodes: function() {
+        return $.ajax({
+            "async": true,
+            "crossDomain": true,
+            "url": URL,
+            "method": "GET",
+            "headers": HEADERS
+        })
+    },
+    getCode: function(pin) {
+        return $.ajax({
+            "async": true,
+            "crossDomain": true,
+            "url": URL + `?q={"pin":${parseInt(pin)}`,
+            "method": "GET",
+            "headers": HEADERS
+        })
+    },
+    generateCode: function() {
+        
+    }
+}
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,11 @@
-const API_KEY = "5c0f48fb5c41c52f7e131f3a";
 
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://doorman-1f48.restdb.io/rest/keys",
-    "method": "GET",
-    "headers": {
-        "content-type": "application/json",
-        "x-apikey": API_KEY,
-        "cache-control": "no-cache"
-    }
-}
-
-$.ajax(settings).done(function (response) {
-    console.log(response);
+$(function () {
+    $codes = $('codes')
+    DB.getCodes().done(function (response) {
+        console.log(response);
+        response.forEach(key => {
+            $codes.append(`<li><strong>${key.name}</strong>[${key.pin}] Expires: ${key.expiry}</li>`)
+        });
+    });
 });
+
