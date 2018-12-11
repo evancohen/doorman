ccfdd0b54c3fcf3a0be6407e0211d02f80a96192
Evan Cohen <evanbtcohen@gmail.com>
2018-12-11 02:00:21 -0800

    flusing out functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
 
+// On Load...
 $(function () {
+
+    // Because it's not supported
+    document.getElementById('expiry').valueAsDate = new Date();
+
     $codes = $('#codes')
+    $("#expiry").val(new Date().toISOString().substring(0, 16))
     DB.getCodes().done(function (response) {
         console.log(response);
+        let options = { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
         response.forEach(key => {
-            $codes.append(`<li data-id="${key.id}"><strong>${key.name}</strong>[${key.pin}] Expires: ${key.expiry}</li>`)
+            let valid = (Date.now() <= Date.parse(key.expiry))? 'valid':'expired'
+            $codes.append(`<tr id="${key['_id']}"><td>${key.name}</td>  
+            <td class="${valid}">${new Date(key.expiry).toLocaleDateString('en-US', options)}</td>
+            <td>[${key.pin}]</td>
+            <td><a href="#" onclick="deleteCode('${key['_id']}')">X</a></dt></tr>`)
         });
     });
 
@@ -13,3 +24,25 @@ $(function () {
     // })
 });
 
+deleteCode = (id) => {
+    DB.deleteCodeByID(id).done(function (results) {
+        $("#" + id).remove()
+    })
+}
+
+let dateOffset = (hours, days, months, years) =>{
+    let date = new Date()
+    date.setHours(date.getHours()+hours)
+    date.setDate(date.getDate()+days)
+    date.setMonth(date.getMonth()+months)
+    date.setFullYear(date.getFullYear()+years)
+    return date
+}
+
+generateCode = (date) => {
+    let date = new Date()
+    date.setHours(date.getHours()+hours_to_be_added); 
+    DB.addCode("test", new Date().toISOString(), 1234).done(function (result) {
+        console.log(result)
+    })
+}
