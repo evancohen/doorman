b491e95328ab9e337f19dad183bc47db5876adfa
Evan Cohen <evanbtcohen@gmail.com>
2018-12-10 16:28:28 -0800

    smoothing out user expereince

--- call.js
+++ call.js
@@ -1,10 +1,11 @@
 exports.handler = function (context, event, callback) {
     let twiml = new Twilio.twiml.VoiceResponse()
 
-    // Call Kartik
     let phoneNumber = context.KARTIK_PHONE_NUMBER;
+    let callerId = event.From || 'No Number';
     let dialParams = { 'callerId': callerId, 'timeLimit': 200 };
 
+    twiml.say("Connecting you now.")
     twiml.dial(dialParams, phoneNumber);
 
     callback(null, twiml)
