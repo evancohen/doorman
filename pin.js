e5b6696d99c946135a2c0294f0727eb91945da6f
Evan Cohen <evanbtcohen@gmail.com>
2018-12-16 14:35:29 -0800

    using speech instead, because the door doesn't send DTMF

--- pin.js
+++ pin.js
@@ -1,52 +1,66 @@
 exports.handler = function (context, event, callback) {
     let request = require("request");
-    
+
     let twiml = new Twilio.twiml.VoiceResponse();
     let restdbURL = context.RESTDB_URL;
     let restdbKey = context.RESTDB_KEY;
     let tries = parseInt(event.tries) || 0;
     console.log("Tries", tries)
 
-    // Ask user for PIN
+    /**
+     * [Helper]
+     * Prompt the user for a pin
+     */
     function gatherPin() {
-        if(tries > 2){
+        if (tries > 2) {
             twiml.say("Too many trys, calling Kartik");
             twiml.redirect('/call');
         }
 
         let gatherConfigs = {
-            numDigits: 4,
-            timeout: 15
+            input: "speech",
+            timeout: 3
         };
         twiml.gather(gatherConfigs)
-            .say('Please enter your PIN,' + ((!tries)? ' or 0 0 0 0 to call Kartik.' : '.'));
+            .say('Please say your PIN. Or say "help" to call Kartik.');
 
         // If the user doesn't enter input, loop
         twiml.redirect(`/pin?tries=${tries}`);
     }
 
-    function validatePin(pin) {
-        twiml.redirect(`/validate?pin=${pin}&tries=${tries}`)
+    /**
+     * [Helper]
+     * Validate the speech
+     */
+
+    function validatePin(speech) {
+        // Make sure we dont get any leftover speech
+        let pinIndex = speech.search(/\d{4}/i);
+        // If there's a 4 digit pin in the utterance
+        if(pinIndex) {
+            let pin = speech.substring(pinIndex, pinIndex + 4);
+            twiml.redirect(`/validate?pin=${pin}&tries=${tries}`)
+        } else {
+            twiml.say("Sorry, I didn't catch that.");
+            twiml.redirect(`/pin?tries=${tries + 1}`);
+        }
     }
 
     console.log("Event:", event);
-    if (event.Digits) {
-        // Call Kartik if the user entered '0000'
-        if(event.Digits == "0000"){
+    console.log("Speech Result", event.SpeechResult)
+    if (event.SpeechResult) {
+        // Call Kartik if the user said help
+        if (event.SpeechResult.toLowerCase().includes("help")) {
             twiml.redirect('/call');
         }
         // If input was detected, look up entered PIN
-        validatePin(event.Digits);
+        validatePin(event.SpeechResult);
 
 
     } else {
         // If no input was sent, use the <Gather> verb to collect user input
         gatherPin();
     }
-
-    // if (tries < 2) {
-    //     twiml.redirect(`/trypin?tries=${tries + 1}`);
-    // }
 
     console.log("about to end...")
     callback(null, twiml);
